43fa5aebfbf80bd0c313235215421de38babaec0
yesa <yesa@yesasurya.com>
2020-04-15 11:56:19 +0700

    Commit5

--- nvme-ioctl.h
+++ nvme-ioctl.h
@@ -7,7 +7,22 @@
 
 #include <linux/types.h>
 #include "linux/nvme_ioctl.h"
-#include "linux/nvme.h"
+
+/* I/O commands */
+enum nvme_opcode {
+	nvme_cmd_flush		= 0x00,
+	nvme_cmd_write		= 0x01,
+	nvme_cmd_read		= 0x02,
+	nvme_cmd_write_uncor	= 0x04,
+	nvme_cmd_compare	= 0x05,
+	nvme_cmd_write_zeroes	= 0x08,
+	nvme_cmd_dsm		= 0x09,
+	nvme_cmd_verify		= 0x0c,
+	nvme_cmd_resv_register	= 0x0d,
+	nvme_cmd_resv_report	= 0x0e,
+	nvme_cmd_resv_acquire	= 0x11,
+	nvme_cmd_resv_release	= 0x15,
+};
 
 int nvme_io(int fd, __u8 opcode, __u64 slba, __u16 nblocks, __u16 control,
 	      __u32 dsmgmt, __u32 reftag, __u16 apptag,
